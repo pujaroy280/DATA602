a07827c63ea864d8356a51a13404d302896322c5
Puja Roy <62675121+pujaroy280@users.noreply.github.com>
2024-02-12 01:15:46 -0500

    Added Week#2 Assigment

---
+++ QOTW2.py
@@ -0,0 +1,38 @@
+"""
+Puja Roy
+QOTW#2
+2/12/24
+"""
+
+#1.   
+
+y = 5
+
+#2. 
+
+first_name = "Puja"
+
+#3.
+
+last_name = "Roy"
+
+#4.
+
+full_name = first_name + " " + last_name
+print(full_name)
+
+# 5. 
+
+hours_worked = 20 
+
+wage_per_hour = 15
+
+total_pay = hours_worked * wage_per_hour
+print(total_pay)
+
+# 6. 
+
+x = 10
+
+y = x**7
+print(y)
